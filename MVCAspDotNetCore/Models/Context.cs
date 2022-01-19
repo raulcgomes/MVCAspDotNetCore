28a8e14a3da986c8948d63d90ceb71a6e3b8da94
Raul Crisostomo Gomes <raul.cgomes92@gmail.com>
2022-01-19 00:25:19 -0300

    First commit

---
+++ MVCAspDotNetCore/Models/Context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCAspDotNetCore.Models
+{
+    public class Context : DbContext
+    {
+        private string connectionString = @"Server=localhost\SQLEXPRESS;Database=MVCAspDotNetCore;Integrated Security=True";
+
+        public DbSet<Categoria> Categorias { get; set; }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: connectionString);
+        }
+    }
+}
---
+++ MVCAspDotNetCore/Models/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCAspDotNetCore.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        [Display(Name = "Descrição")]
+
+        public string Descricao { get; set; }
+
+        [Range(1, 10, ErrorMessage = "Valor fora do range")]
+        public int Quantidade { get; set; }
+
+        public int CategoriaId { get; set; }
+
+        public Categoria Categoria { get; set; }
+    }
+}
